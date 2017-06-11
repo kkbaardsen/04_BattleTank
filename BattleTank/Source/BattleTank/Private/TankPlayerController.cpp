afb251abf57d7549ccb5203ae297a666128454e6
Kim K Baardsen <kkbaardsen@gmail.com>
2017-06-11 16:10:38 +0200

    Lecture 121: Added BeginPlay() using override from parent class to TankPlayerController with logs for if the PlayerController is possessing a tank or not

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,7 +3,23 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing Tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *(ControlledTank->GetName()));
+	}
+	}
+	
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
-	return Cast<ATank>(GetPawn());
+	return Cast<ATank>(GetPawn());	
 }
