909ef940bc0a685962e471ae46ede31886f3722b
Kim K Baardsen <kkbaardsen@gmail.com>
2017-06-11 21:40:52 +0200

    Lecture 133: Added Barrel location(set using Blueprint) to TankAimingComponent thereby giving us barrel location and hit location for future projectile flight calculations

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -25,4 +25,9 @@ public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	
 	void AimAt(FVector HitLocation);
+
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
+private:
+	UStaticMeshComponent* Barrel = nullptr;
 };
